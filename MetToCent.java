94e213d0d2cf43917c8b77d6c6060a98ca652cb1
Harsh-Builds <harshsaini888555@gmail.com>
2025-09-21 22:41:41 +0530

    creates a simple program to convert meter to centimeter in JAVA

---
+++ MetToCent.java
@@ -0,0 +1,11 @@
+ class MetToCent {
+    public static void main(String args []){
+        double meters ;
+        double centimeters;
+
+        meters = 0.75;
+        centimeters = meters*100;
+
+        System.out.println(meters + " meter is " + centimeters + " centimeter");
+    }
+}
