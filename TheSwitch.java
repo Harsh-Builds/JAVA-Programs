04dbce4662be23ac25816ceaa45e3e39a12af6d7
Harsh-Builds <harshsaini888555@gmail.com>
2025-09-29 20:15:16 +0530

    Creates a program using Switch statement

---
+++ TheSwitch.java
@@ -0,0 +1,46 @@
+ class TheSwitch {
+    
+    public static void main(String[] args){
+
+        // Using Switch statement 
+        // Print Days in a week
+        // for(num = 1; num <= 7; num ++){
+            
+        // use Switch 
+        int num = 'A';
+            switch(num){
+
+                case 'A' :
+                System.out.println("Sunday");
+                break;
+
+                case 2 :
+                System.out.println("Monday");
+                break;
+
+                case 3 :
+                System.out.println("Tuesday");
+                break;
+
+                case 4 :
+                System.out.println("wednesday");
+                break;
+
+                case 5 :
+                System.out.println("Thursday");
+                break;
+
+                case 6 :
+                System.out.println("Friday");
+                break;
+
+                case 7 :
+                System.out.println("Saturday");
+                break;
+
+                default :
+                 System.out.println("No case matched");
+            }
+        // }
+    }
+}
